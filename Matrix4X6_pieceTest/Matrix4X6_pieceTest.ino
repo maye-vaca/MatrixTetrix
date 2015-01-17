adf5790a15718f8ef195ac6f10af6f3df45fbca7
maye-vaca <madeleyne.vaca@gmail.com>
2015-01-17 09:15:15 -0500

    fixed glitches with routinely changing shapes in the matrix

--- Matrix4X6_pieceTest/Matrix4X6_pieceTest.ino
+++ Matrix4X6_pieceTest/Matrix4X6_pieceTest.ino
@@ -22,37 +22,46 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  //drawPiece(random(1,2));
-  
-  drawMap();
-  //delay(500);
-  //alloff();
-  //delay(500);
+  drawPiece(1);
+  int x = 0;
+  do {
+    drawMap();
+    x++;
+    delay(1);
+  } 
+  while (x < 200);
+  alloff();
+  delay(250);
 }
 
 void alloff() {
   for (int i = 0; i < numRows; i++) {
-    digitalWrite(row[i], LOW);
-  }
-  for (int j = 0; j < numCols; j++) {
-    digitalWrite(col[j], HIGH); 
+    for (int j = 0; j < numCols; j++) {
+      digitalWrite(col[j], HIGH); 
+      digitalWrite(row[i], LOW);
+      grid[i][j] = 0;
+    }
   }
 }
 
 void drawMap() {
-  alloff();
-  Serial.print("GRID:\n");
+  //Serial.print("GRID:\n");
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 6; j++) {
-      if(grid[i][j]) {
+      if(grid[i][j] == 1) {
         digitalWrite(row[i], HIGH);
-        digitalWrite(col[i], LOW); 
+        digitalWrite(col[j], LOW); 
+      } 
+      else {
+        digitalWrite(col[j],HIGH);
       }
-      
-      Serial.print(grid[i][j] + " ");
-      delay(100);
+      //delay(5);
+      //Serial.print(grid[i][j]);
+      //delay(100);
     }
-    Serial.println();
+    digitalWrite(row[i], LOW);
+    delay(5);
+    //Serial.println();
   } 
 
 }
@@ -61,23 +70,48 @@ void drawPiece(int num) {
   Serial.println(num);
   if (enoughSpace(num)) {
     switch (num) {
-      case 1: 
-        for (int i = 0; i < 4; i++) {
-          grid[1][i] = 1; 
-        }
-        break;
-     case 2:
-       grid[0][2] = grid[0][3] = grid[1][2] = grid[1][3] = 1;
-       break;
-     default:
-       break;
+    case 1: 
+      for (int i = 1; i < 5; i++) {
+        grid[0][i] = 1; 
+      }
+      break;
+    case 2:
+      grid[0][2] = grid[0][3] = 1;
+      grid[1][2] = grid[1][3] = 1;
+      break;
+    case 3:
+      grid[0][3] = 1;
+      grid[1][2] = grid[1][3] = 1;
+      grid[2][3] = 1;
+      break;
+    case 4:
+      grid[0][2] = grid[0][3] = grid[0][4] = 1;
+      grid[1][4] = 1;
+      break;
+    case 5:
+      grid[0][2] = grid[0][3] = grid[0][4] = 1;
+      grid[1][2] = 1;
+      break;
+    case 6:
+      grid[0][2] = 1;
+      grid[1][2] = grid[1][3] = 1;
+      grid[2][3] = 1;
+      break;
+        case 7:
+      grid[0][3] = 1;
+      grid[1][2] = grid[1][3] = 1;
+      grid[2][2] = 1;
+    default:
+      break;
     }
   }
 }
 
 boolean enoughSpace(int num) {
-   return true; 
+  return true; 
 }
+
+
 
 
 
